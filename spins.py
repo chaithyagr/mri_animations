15241550d5d04157cbdd5665dad01a4a84ce39da
Chaithya G R <chaithyagr@gmail.com>
2023-03-17 09:35:55 +0100

    Added temp

--- spins.py
+++ spins.py
@@ -1,11 +1,13 @@
 from manim import *
 #from manim.opengl import *
 import numpy as np
-from manim_slides import Slide, ThreeDSlide
+from manim_slides import ThreeDSlide
 
 N = 3
-grid = np.asarray(np.meshgrid(np.arange(0, N), np.arange(0, N), np.arange(0, N),))
-RES = 8
+grid = np.asarray(np.meshgrid(np.arange(-N//2+1, N//2+1), np.arange(-N//2+1, N//2+1), np.arange(-N//2+1, N//2+1)))
+RES = 2
+np.random.seed(0)
+
 
 def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED, sphere_color=BLUE):
     sphere = Sphere(
@@ -16,7 +18,7 @@ def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED, sphe
                 v_range=[0, TAU],
             )
     sphere.set_color(sphere_color)
-    arrow_radius = radius * 3
+    arrow_radius = radius * 6
     radius_in_plane = arrow_radius*np.cos(angle_in_z)
     radius_out_plane = arrow_radius*np.sin(angle_in_z)
     delta = [radius_in_plane*np.sin(angle_in_xy), radius_in_plane*np.cos(angle_in_xy), radius_out_plane]
@@ -28,19 +30,20 @@ def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED, sphe
                 resolution=RES,
                 color=color,
         )
-    M0 = Group(sphere, arrow)
-    return M0
+    return arrow, sphere
 
 class ThreeDSpinsRandomOrientation(ThreeDSlide):
     def construct(self):
         axes = ThreeDAxes()
+        arrows = np.empty((N, N, N), dtype=object)
+        spheres =  np.empty((N, N, N), dtype=object)
         for i in range(N):
             for j in range(N):
                 for k in range(N):
                     arrow_angle = np.random.random()*2*PI
                     angle_z = np.random.random()*PI/2 
-                    M0 = get_spin(grid[:, i, j, k], arrow_angle, angle_in_z=angle_z, radius=0.2)
-                    self.add(axes, M0)
+                    arrows[i, j, k], spheres[i, j, k] = get_spin(grid[:, i, j, k], arrow_angle, angle_in_z=angle_z, radius=0.5)
+        self.add(*arrows, *spheres)
         self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
         self.begin_3dillusion_camera_rotation(rate=2)
@@ -49,22 +52,65 @@ class ThreeDSpinsRandomOrientation(ThreeDSlide):
         self.next_slide()
         
 class ThreeDSpinsPrecession(ThreeDSlide):
+    def do_camera_rotation(self):
+        self.begin_ambient_camera_rotation(rate=DEGREES*5, about='theta')
+        self.wait(DEGREES*180)
+        self.stop_ambient_camera_rotation()
+        self.begin_ambient_camera_rotation(rate=-DEGREES*5, about='phi')
+        self.wait(DEGREES*180)
+        self.stop_ambient_camera_rotation()
+        
+        self.begin_ambient_camera_rotation(rate=-DEGREES*5, about='theta')
+        self.wait(DEGREES*180)
+        self.stop_ambient_camera_rotation()
+        self.begin_ambient_camera_rotation(rate=DEGREES*5, about='phi')
+        self.wait(DEGREES*180)
+        self.stop_ambient_camera_rotation()
+        
+        
     def construct(self):
         axes = ThreeDAxes()
-        def rotate(d, dt):
-            d.rotate(dt*5, about_point=d.get_center())
-            
+        self.add(axes)
+        def rotate(d, dt, about='center'):
+            if about == 'center':
+                d.rotate(dt*5, about_point=d.get_center())
+            elif about == 'start':
+                d.rotate(dt*5, about_point=d.get_start())
+        
+        arrows = np.empty((N, N, N), dtype=object)
+        spheres =  np.empty((N, N, N), dtype=object)
+        Animations = []
         for i in range(N):
             for j in range(N):
                 for k in range(N):
                     arrow_angle = np.random.random()*2*PI
-                    M0 = get_spin(grid[:, i, j, k], arrow_angle, radius=0.1)
-                    M0.add_updater(rotate)
-                    self.add(axes, M0)
-                    
+                    arrows[i, j, k], spheres[i, j, k] = get_spin(grid[:, i, j, k], arrow_angle, radius=0.1)
+                    arrows[i, j, k].add_updater(rotate)
+        self.add(*arrows.flatten(), *spheres.flatten())
         self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.wait(2*PI)        
+        #self.start_loop()
+        #self.do_camera_rotation() 
+        #self.end_loop()  
+        
+        self.remove(*spheres.flatten())
+        Animations = []
+        for i in range(N):
+            for j in range(N):
+                for k in range(N):
+                    arrow = arrows[i, j, k]
+                    arrow.thickness = 0.01
+                    line = Line(start=arrow.get_start(), end=[0,0,0])
+                    Animations.append(MoveAlongPath(arrow, line))
+        self.play(*Animations, rate_func=linear, run_time=2)
         
         
+        self.move_camera(phi=90*DEGREES, theta=45 * DEGREES, zoom=3, run_time=2)
+        self.start_loop() 
+        self.wait(PI)
+        self.end_loop()                    
+        
+        
+        
+
 
