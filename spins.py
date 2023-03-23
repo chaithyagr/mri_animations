6a0af57a41151927230fa3b8b466033a73becd07
chaithyagr <chaithyagr@gitlab.com>
2023-03-23 09:58:08 +0100

    Some devcontainer

--- spins.py
+++ spins.py
@@ -1,19 +1,20 @@
 from manim import *
 import numpy as np
 
-N = 10
+N = 3
 grid = np.asarray(np.meshgrid(np.arange(0, N), np.arange(0, N), np.arange(0, N),))
 
-def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED):
+def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED, color_sphere=BLUE):
     sphere = Sphere(
                 center=location,
                 radius=radius,
-                resolution=(8, 8),
+                resolution=(16, 16),
                 u_range=[0.001, PI - 0.001],
                 v_range=[0, TAU],
-                color=color,
+                sheen_factor=0,
             )
-    arrow_radius = radius * 3
+    sphere.set_color(color_sphere)
+    arrow_radius = radius * 4
     radius_in_plane = arrow_radius*np.cos(angle_in_z)
     radius_out_plane = arrow_radius*np.sin(angle_in_z)
     delta = [radius_in_plane*np.sin(angle_in_xy), radius_in_plane*np.cos(angle_in_xy), radius_out_plane]
@@ -22,7 +23,7 @@ def get_spin(location, angle_in_xy, angle_in_z=PI/3, radius=0.2, color=RED):
     arrow = Arrow3D(
                 start=start_location,
                 end=end_location,
-                resolution=2,
+                resolution=8,
                 color=color,
         )
     M0 = Group(sphere, arrow)
@@ -36,12 +37,12 @@ class ThreeDSpinsRandomOrientation(ThreeDScene):
                 for k in range(N):
                     arrow_angle = np.random.random()*2*PI
                     angle_z = np.random.random()*PI/2 
-                    M0 = get_spin(grid[:, i, j, k], arrow_angle, angle_in_z=angle_z, radius=0.25)
+                    M0 = get_spin(grid[:, i, j, k], arrow_angle, angle_in_z=angle_z, radius=0.1)
                     self.add(axes, M0)
         self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
         self.begin_3dillusion_camera_rotation(rate=2)
-        self.wait(PI*10)
+        self.wait()
         self.stop_3dillusion_camera_rotation()
         
         
@@ -64,4 +65,20 @@ class ThreeDSpinsPrecession(ThreeDScene):
         self.wait(2*PI)        
         
         
+class ParaSurface(ThreeDScene):
+    def func(self, u, v):
+        return np.array([np.cos(v)*np.sin(v), np.sin(u)*np.sin(v), u])
 
+    def construct(self):
+        axes = ThreeDAxes()
+        surface = Surface(
+            lambda u, v: axes.c2p(*self.func(u, v)),
+            u_range=[-PI, PI],
+            v_range=[0, TAU],
+            resolution=8,
+        )
+        #surface.set_color(BLUE)
+        self.set_camera_orientation(theta=70 * DEGREES, phi=75 * DEGREES)
+        self.add(axes, surface)
+        self.wait()
+        self.set_camera_orientation(theta=15 * DEGREES, phi=15 * DEGREES)
